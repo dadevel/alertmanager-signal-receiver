b4ea3e868940187ce1dd3095e95f5685b97408ad
Daniel <dadevel@disroot.org>
2020-04-18 18:57:48 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM gradle:jre14 as signal
+WORKDIR /build
+RUN git clone --depth 1 "https://github.com/AsamK/signal-cli.git" .
+RUN ./gradlew build && ./gradlew installDist
+
+FROM golang:alpine as receiver
+WORKDIR /build
+COPY . .
+RUN go build
+
+FROM openjdk:14-alpine
+WORKDIR /app
+COPY --from=signal /build/build/install/signal-cli/bin/ ./bin/
+COPY --from=signal /build/build/install/signal-cli/lib/ ./lib/
+COPY --from=receiver /build/alertmanager-signal-receiver ./bin/
+RUN apk add --no-cache libqrencode
+RUN mkdir ./data && chown -R nobody:nogroup ./data
+USER nobody:nogroup
+ENV PATH /app/bin:$PATH
+VOLUME /app/data
+ENTRYPOINT ["alertmanager-signal-receiver"]
+
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "os/exec"
+    "strings"
+
+    "github.com/prometheus/alertmanager/template"
+)
+
+var (
+    sender string
+    group string
+    dataDir = "./data"
+    address = ":8080"
+    logger = log.New(os.Stderr, "", 0)
+)
+
+func healthz(res http.ResponseWriter, req *http.Request) {
+    io.WriteString(res, "OK\n")
+}
+
+func alert(res http.ResponseWriter, req *http.Request) {
+    data, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        writeError(res, fmt.Errorf("failed to read request body: %w", err), http.StatusBadRequest)
+        return
+    }
+    payload := &template.Data{}
+    err = json.Unmarshal(data, payload)
+    if err != nil {
+        writeError(res, fmt.Errorf("failed to unmarshal payload: %w", err), http.StatusBadRequest)
+        return
+    }
+    var message []string
+    for _, alert := range payload.Alerts {
+        name := mapGet(alert.Labels, "alertname", "unknown alert")
+        instance := mapGet(alert.Labels, "instance", "unknown instance")
+        message = append(message, name + " " + alert.Status + " at " + instance)
+    }
+    err = sendMessage(message)
+    if (err != nil) {
+        writeError(res, fmt.Errorf("failed to send message: %w", err), http.StatusInternalServerError)
+        return
+    }
+}
+
+func mapGet(mapping map[string]string, key string, defaultValue string) string {
+    value, ok := mapping[key]
+    if ok {
+        return value
+    }
+    return defaultValue
+}
+
+func sendMessage(message []string) error {
+    logger.Printf("sending message: %s", strings.Join(message, ", "))
+    var buffer bytes.Buffer
+    cmd := exec.Command("signal-cli", "--config", dataDir, "--username", sender, "send", "--group", group)
+    cmd.Stdin = strings.NewReader(strings.Join(message, "\n"))
+    cmd.Stdout = &buffer
+    cmd.Stderr = &buffer
+    err := cmd.Run()
+    if err != nil {
+        return fmt.Errorf("%v: %w", buffer.String(), err)
+    }
+    return nil
+}
+
+func writeError(res http.ResponseWriter, err error, code int) {
+    logger.Println(err)
+    http.Error(res, err.Error(), code)
+}
+
+func loadEnvironment() {
+    sender = os.Getenv("SIGNAL_SENDER")
+    group = os.Getenv("SIGNAL_GROUP")
+    if sender == "" || group == "" {
+        logger.Fatal("SIGNAL_SENDER and/or SIGNAL_GROUP environment variables are not set")
+    }
+    if os.Getenv("SIGNAL_DATA") != "" {
+        dataDir = os.Getenv("SIGNAL_DATA")
+    }
+    if os.Getenv("WEBHOOK_ADDRESS") != "" {
+        address = os.Getenv("WEBHOOK_ADDRESS")
+    }
+}
+
+func startServer() {
+    http.HandleFunc("/healthz", healthz)
+    http.HandleFunc("/alert", alert)
+    logger.Printf("listening on %v", address)
+    logger.Fatal(http.ListenAndServe(address, nil))
+}
+
+func main() {
+    loadEnvironment()
+    startServer()
+}
+
